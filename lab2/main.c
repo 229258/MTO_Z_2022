5cf9879fa006032dff8edecc1399d0057e0ca93d
Wojciech Grzybek <229258@edu.p.lodz.pl>
2022-10-09 12:38:58 +0200

    Recovered support for format #k

--- lab2/main.c
+++ lab2/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == '.') && (isdigit(format_string[i+2]) != 0) && (format_string[i+3] == 'k')){
-			i = i + 3;
+	
+	
+		if((format_string[i] == '#') && (format_string[i+1] == 'k')) {
+			
+			i++;
 			printf("%s",param);
 			
 			for (int j = 0; j < strlen(format_string); j++) {
@@ -14,12 +18,34 @@ int my_printf(char *format_string, char *param){
 					format_string[j] -= 32;
 
 			}
+		
+			
+		}else if((format_string[i] == '#') && (format_string[i+1] == '.')) {
+			if ((isdigit(format_string[i+2]) != 0) && (format_string[i+3] == 'k')) {
+				
+
+		
+			
+				i = i + 3;
+				printf("%s",param);
+				
+				for (int j = 0; j < strlen(format_string); j++) {
+					if ((format_string[j] >= 'A') && (format_string[j] <= 'Z'))
+						format_string[j] += 32;
+					else if ((format_string[j] >= 'a') && (format_string[j] <= 'z'))
+						format_string[j] -= 32;
+
+				}
+			}
 		}else
 			putchar(format_string[i]);
 	}
 	puts("");
 	return 0;
 }
+
+
+
 
 int main(int argc, char *argv[]){
 	char buf[1024],buf2[1024];
