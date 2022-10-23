c4b3c2247b1492246a7829c6375d46780a4ef1fe
Wojciech Grzybek <229258@edu.p.lodz.pl>
2022-10-23 11:17:10 +0200

    Added 3 new tests.

--- lab3/main.c
+++ lab3/main.c
@@ -40,7 +40,7 @@ int my_printf(char *format_string, char *param){
 				}
 				//format_string[j] = '\n';
 			}
-		} else if((format_string[i] == '#') && (isdigit(format_string[i+1]) != 0) && (format_string[i+3] == 'k')) {
+		} else if((format_string[i] == '#') && (isdigit(format_string[i+1]) != 0) && (format_string[i+2] == 'k')) {
 				// terminator required because of protection for segmentation fault
 				char value_with_terminator[2] = {format_string[i + 1], '\0'};
 				int number_of_characters = atoi(value_with_terminator);
