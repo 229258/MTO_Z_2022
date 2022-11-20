6bdf749aa65e251b88b4a0aeb89fe8db5d9539bb
229258 <229258@edu.p.lodz.pl>
2022-11-20 11:05:38 +0100

    init commit, code taken from lab4

---
+++ lab5/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <stdlib.h>
+#include <ctype.h>
+
+void reverse(char *string) {
+	int string_length = strlen(string);
+
+	for (int i = 0; i < string_length / 2; ++i) {
+		char temp = string[i];
+		string[i] = string[string_length - i - 1];
+		string[string_length - i - 1] = temp;
+	}
+}
+
+int my_printf(char *format_string, char *param){
+	for(int i=0;i<strlen(format_string);i++){
+		if((format_string[i] == '#') && (format_string[i+1] == 'g')){
+			i++;
+
+			int j = 0;
+			for (j = 0; j < strlen(param); ++j) {
+				if (!isdigit(param[j]))
+					break;
+			}
+						
+			if (j < strlen(param)) {
+				puts("");
+				return -1;
+			} else {
+				reverse(param);
+				printf("%d", atoi(param));
+			}
+		} else
+			putchar(format_string[i]);
+	}
+	puts("");
+	return 0;
+}
+
+int main(int argc, char *argv[]){
+	char buf[1024],buf2[1024];
+	while(gets(buf)){
+		gets(buf2);
+		my_printf(buf,buf2);
+	}
+	return 0;
+}
